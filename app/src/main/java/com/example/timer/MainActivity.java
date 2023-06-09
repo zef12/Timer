d6c0934835a76009a07a775cd5120e28797c7571
zef12 <sersevest@gmail.com>
2023-06-09 16:27:01 +0300

    передача в шторку уведомления

--- app/src/main/java/com/example/timer/MainActivity.java
+++ app/src/main/java/com/example/timer/MainActivity.java
@@ -1,11 +1,23 @@
 package com.example.timer;
 
+import static android.app.job.JobInfo.PRIORITY_HIGH; //TestNotification
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat; //TestNotification
+
+import android.app.NotificationChannel; //TestNotification
+import android.app.PendingIntent; //TestNotification
+import android.content.Context; //TestNotification
+import android.content.Intent; //TestNotification
+import android.os.Build; //TestNotification
+import android.app.NotificationManager; //TestNotification
 
 import java.text.SimpleDateFormat; //Timer
 import java.util.Calendar; //Timer
 import java.util.Timer; //Timer
 import java.util.TimerTask; //Timer
+
+import android.content.Intent;
 import android.os.Bundle; //Timer
 import android.view.View; //Timer
 import android.view.View.OnClickListener; //Timer
@@ -14,15 +26,14 @@ import android.widget.CheckBox; //Timer
 import android.widget.TextView; //Timer
 import android.app.Activity; //Timer
 
-
-
 public class MainActivity extends AppCompatActivity {
-
-    // ОБъявляем используемые объекты:
+    private NotificationManager notificationManager; //TestNotification
+    private static final int NOTIFY_ID = 1; //TestNotification
+    private static final String CHANNEL_ID = "CHANNEL_ID"; //TestNotification
+// ОБъявляем используемые объекты:
     CheckBox mCheck;
     Button mStart, mStop;
     TextView mCount;
-
     Timer timer;
     TimerTask mTimerTask;
 
@@ -31,11 +42,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Связываемся с элементами пользовательского интерфейса:
+// Связываемся с элементами пользовательского интерфейса:
         mCheck = (CheckBox)findViewById(R.id.single_shot);
         mStart = (Button)findViewById(R.id.start);
         mStop = (Button)findViewById(R.id.stop);
         mCount = (TextView)findViewById(R.id.count);
+        notificationManager = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE); //TestNotification
 // Настраиваем слушателя нажатий по кнопке "Старт":
         mStart.setOnClickListener(new OnClickListener(){
             @Override
@@ -47,17 +59,36 @@ public class MainActivity extends AppCompatActivity {
 // выполняем задачу MyTimerTask, описание которой будет ниже:
                 timer = new Timer();
                 mTimerTask = new MyTimerTask();
+            /*    Intent intent = new Intent(getApplicationContext(), MainActivity.class); //*TestNotification
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                PendingIntent pendingIntent =
+                        PendingIntent.getActivity(getApplicationContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                NotificationCompat.Builder notificationBuilder =
+                        new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
+                                .setAutoCancel(false)
+                                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                                .setWhen(System.currentTimeMillis())
+                                .setContentIntent(pendingIntent)
+                                .setContentTitle("text1")
+                                .setContentText("text1")
+                                .setPriority(PRIORITY_HIGH);
+                createChannelIfNeeded(notificationManager);
+                notificationManager.notify(NOTIFY_ID, notificationBuilder.build()); //*///TestNotification
 
                 if(mCheck.isChecked()){
 
 // Выполняем действие с задержкой 1 секунда:
-                    timer.schedule(mTimerTask, 1000);
+//                    timer.schedule(mTimerTask, 1000);
+// Выполняем переход с задержкой 5 секунда:
+                    timer.schedule(mTimerTask, 5000);
+
 
                 }else{
 // После задержки одна секунда, повторяем действие таймера каждую секунду:
-                    timer.schedule(mTimerTask, 1000, 1000);
+                    timer.schedule(mTimerTask, 5000, 5000);
                 }
-            }});
+            }
+        });
 
 // Кнопка "Остановить" отменяет действие таймера:
         mStop.setOnClickListener(new OnClickListener(){
@@ -68,10 +99,20 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    // Метод для описания того, что будет происходить при работе таймера (задача для таймера):
+
+    public static void createChannelIfNeeded(NotificationManager manager) { //*TestNotification
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, CHANNEL_ID, NotificationManager.IMPORTANCE_DEFAULT);
+            manager.createNotificationChannel(notificationChannel);
+        }
+    } // *//TestNotification
+
+// Метод для описания того, что будет происходить при работе таймера (задача для таймера):
     class MyTimerTask extends TimerTask {
         @Override
         public void run() {
+           /* Intent intent = new Intent(MainActivity.this,Second.class);
+            startActivity(intent);*/
 
 // Берем дату и время с системного календаря:
             Calendar calendar = Calendar.getInstance();
@@ -80,12 +121,28 @@ public class MainActivity extends AppCompatActivity {
 // Преобразуем информацию в строковые данные:
             final String strDate = simpleDateFormat.format(calendar.getTime());
             runOnUiThread(new Runnable(){
-
-                // Отображаем информацию в текстовом поле count:
+// Отображаем информацию в текстовом поле count:
                 @Override
                 public void run() {
                     mCount.setText(strDate);
-                }});
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class); //*TestNotification
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    PendingIntent pendingIntent =
+                            PendingIntent.getActivity(getApplicationContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                    NotificationCompat.Builder notificationBuilder =
+                            new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
+                                    .setAutoCancel(false)
+                                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                                    .setWhen(System.currentTimeMillis())
+                                    .setContentIntent(pendingIntent)
+                                    .setContentTitle(strDate)
+                                    .setContentText(strDate)
+                                    .setPriority(PRIORITY_HIGH);
+                    createChannelIfNeeded(notificationManager);
+                    notificationManager.notify(NOTIFY_ID, notificationBuilder.build()); //*//TestNotification
+                }
+
+            });
         }
     }
 }
